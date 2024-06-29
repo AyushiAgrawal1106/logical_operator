a1acd1bb1d589e4bd526ea12f8978b1c5c91ac26
ASUS <ayushiagrwl11@gmail.com>
2024-06-29 23:07:34 +0530

    For Logical Operator assignment

--- src/main/scala/L02.scala
+++ src/main/scala/L02.scala
@@ -1,6 +1,6 @@
 object L02 {
   def main(args:Array[String]):Unit={
-    val a:Int=(-15)
+    val a:Int= -15
     if(a<(-10) || a>10){
       print("Number is either less than -10 or greater than 10")
     }
