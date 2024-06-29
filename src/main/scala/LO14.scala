63d89cfe8e8ffb5a75566879b2f423f78b3c2f25
ASUS <ayushiagrwl11@gmail.com>
2024-06-29 23:06:55 +0530

    For Logical Operator assignment

--- src/main/scala/LO14.scala
+++ src/main/scala/LO14.scala
@@ -1,6 +1,6 @@
 object LO14 {
   def main(args:Array[String]):Unit={
-    val a=(-6)
+    val a= -6
     if(a<0 && a%2==0){
       print("Number is non positive and even")
     }
--- src/main/scala/LO7.scala
+++ src/main/scala/LO7.scala
@@ -1,6 +1,6 @@
 object LO7 {
   def main(args:Array[String]):Unit={
-    val a=(-7)
+    val a= -7
     if(a<0 && a%2!=0){
       print("Number is negative and odd")
     }
